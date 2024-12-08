cf668cbf5b4baaccacbfa069ba994ad7d343164f
kokofixcomputers <github@kokofixcomputers.serv00.net>
2024-12-08 10:23:50 -0800

    :lipstick: style: Colour Works! Now just finish off
    Signed-off-by: kokodev <koko@kokodev.cc>

--- handler.py
+++ handler.py
@@ -33,7 +33,7 @@ def create_container():
         "PASSWORD_ACCESS": "true",
         "USER_PASSWORD": password,
         "USER_NAME": username,
-        "WELCOME_MESSAGE": "\033[36mWelcome to your temporary environment.\033[0m \033[1mPLEASE REFRAIN FROM USING CONFIDENTIAL INFORMATION IN THIS ENVIROMENT.\033[0m\nAdmins can see and connect to this container.\n\033[33mPlease also refrain from using this environment for anything illegal or unethical.\033[0m Thank you for your cooperation.\n\nTemporary SSH Server made by kokofixcomputers\n\n\n\033[1mWARNING: By using this environment you agree not to use this container for anything illegal or unehtical.\n\n\033[0m",
+        "WELCOME_MESSAGE": "\033[36mWelcome to your temporary environment.\033[0m\nTemporary SSH Server made by kokofixcomputers\nGitHub: https://github.com/kokofixcomputers/TemporarySSHSessionManager.git \n\033[1;32mSUDO Note: Sudo access is enabled.\033[0m\n\033[1mPLEASE REFRAIN FROM USING CONFIDENTIAL INFORMATION IN THIS ENVIROMENT.\033[0m\nPlease note: admins can see and connect to this container.\n\033[33mPlease also refrain from using this environment for anything illegal or unethical.\n\033[0m Thank you for your cooperation.\n\033[1mWARNING: By using this environment you agree not to use this container for anything illegal or unehtical.\n\n\033[0m",
     }
 
     # Create and run the container

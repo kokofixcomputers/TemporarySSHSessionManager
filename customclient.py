2fe8249a54fba436863e4305ef3d2c5f7504b7ca
kokofixcomputers <github@kokofixcomputers.serv00.net>
2025-01-30 20:27:26 -0800

    :bug: fix: Incorrect url causes errors
    Signed-off-by: kokodev <koko@kokodev.cc>

--- customclient.py
+++ customclient.py
@@ -3,7 +3,7 @@ import requests
 from InquirerPy import prompt
 from InquirerPy.base.control import Choice
 import os
-
+import argparse
 
 api_key = keyring.get_password("stmclient", "api_key")
 host = keyring.get_password("stmclient", "host")
@@ -12,20 +12,28 @@ if api_key is None:
     host = input("Enter base URL for your instance: ")
     keyring.set_password("stmclient", "api_key", api_key)
     keyring.set_password("stmclient", "host", host)
-containers = requests.get(host + "/api/get_user_containers", headers={"Authorization": api_key}).json()
 
-# Use InquirerPy to prompt the user to select a container
-#[
-#  {
-#    "active": 1,
-#    "exposed_port": 2612,
-#    "hostname": "hosting.kokodev.cc",
-#    "name": "naughty_williamson",
-#    "password": "mid4Xqwn1bZc",
-#    "port": 2491,
-#    "username": "crazy_awesome"
-#  }
-#]
+parser = argparse.ArgumentParser(description="Process some options.", add_help=False)
+
+parser.add_argument('--credential-reset', action='store_true', help='Reset credentials')
+
+args = parser.parse_args()
+if getattr(args, 'credential_reset'):
+    keyring.delete_password("stmclient", "api_key")
+    keyring.delete_password("stmclient", "host")
+    api_key = input("Enter API key: ")
+    host = input("Enter base URL for your instance: ")
+    keyring.set_password("stmclient", "api_key", api_key)
+    keyring.set_password("stmclient", "host", host)
+    print("Credentials reset successfully.")
+    exit(0)
+    
+containers = requests.get(host + "/api/get_user_containers", headers={"Authorization": api_key})
+if containers.status_code == 404:
+    print("A Incorrect API URL Has been provided. Please check your url. Make sure it includes https:// or http:// and the port number.")
+containers = containers.json()
+
+
 choices = [Choice(name=f"{container['name']} ({container['username']}@{container['hostname']})", value=container) for container in containers if container['active'] == 1]
 selected_container = prompt([
     {

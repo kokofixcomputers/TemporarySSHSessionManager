de93bb3a6f547328b49e2e9bd75b2a848ffadb72
kokofixcomputers <github@kokofixcomputers.serv00.net>
2025-02-01 16:52:48 -0800

    :bug: fix: fix bugs
    Signed-off-by: kokodev <koko@kokodev.cc>

--- main.py
+++ main.py
@@ -186,7 +186,7 @@ def generate_api_key():
         c.execute('SELECT * FROM api_key WHERE user=?', (session['username'],))
         if c.fetchone() is not None:
             # Get the user's API Key
-            api = c.execute('SELECT api_key FROM api_key WHERE user=?', (session['username'],)).fetchone()
+            api = c.execute('SELECT api_key FROM api_key WHERE user=?', (session['username'],)).fetchone()[0]
             return f"<p>{api}</p>"
         else:
             # Generate a new API key
